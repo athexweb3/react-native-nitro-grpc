0ef7363c526dd6fa7e0d48665d4625d6912f6b1a
Athex Web3 <athexweb3@gmail.com>
2025-12-06 01:17:27 +0600

    chore: rename to react-native-nitro-grpc and remove workflows

--- example/metro.config.js
+++ example/metro.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const { getDefaultConfig } = require('@react-native/metro-config');
 const { withMetroConfig } = require('react-native-monorepo-config');
 
-const pkgroot = path.resolve(__dirname, 'packages', 'react-native-grpc');
+const pkgroot = path.resolve(__dirname, 'packages', 'react-native-nitro-grpc',);
 
 /**
  * Metro configuration
