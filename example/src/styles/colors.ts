e73ff400527fc8082c015b56ab21ab7f8a097b61
orvenweb3 <orvenweb3@gmail.com>
2025-12-06 13:28:47 +0600

    style: auto-format code with prettier

--- example/src/styles/colors.ts
+++ example/src/styles/colors.ts
@@ -1,9 +1,9 @@
 export const colors = {
-    blue: '#007AFF',
-    white: '#ffffff',
-    gray: '#D1D1D6',
-    green: '#34c759',
-    red: '#ff3b30',
-    black: '#000000',
-    lightGray: '#F2F2F7',
+  blue: '#007AFF',
+  white: '#ffffff',
+  gray: '#D1D1D6',
+  green: '#34c759',
+  red: '#ff3b30',
+  black: '#000000',
+  lightGray: '#F2F2F7',
 };
--- example/src/tests/grpc_tests.ts
+++ example/src/tests/grpc_tests.ts
@@ -2,11 +2,11 @@ import { expect } from 'chai';
 import { GrpcClient } from 'react-native-nitro-grpc';
 
 export const grpc_tests = {
-    'stub test: module exists': () => {
-        expect(GrpcClient).to.not.be.undefined;
-    },
-    'stub test: has function stub': () => {
-        // This is just a placeholder until we have real methods
-        expect(typeof GrpcClient).to.equal('object');
-    }
+  'stub test: module exists': () => {
+    expect(GrpcClient).to.not.be.undefined;
+  },
+  'stub test: has function stub': () => {
+    // This is just a placeholder until we have real methods
+    expect(typeof GrpcClient).to.equal('object');
+  },
 };
