119099f2971f0c9271fc4f981b71033280521955
Athex Web3 <athexweb3@gmail.com>
2025-12-18 18:44:27 +0600

    feat: Add gRPC Android Prefab module with prebuilt artifacts

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,13 +1,6 @@
-const path = require('path');
-const { getConfig } = require('react-native-builder-bob/babel-config');
-const pkg = require('../package.json');
-
-const root = path.resolve(__dirname, '..');
-
-module.exports = getConfig(
-  {
-    presets: ['module:@react-native/babel-preset'],
-    plugins: ['@babel/plugin-transform-class-static-block'],
-  },
-  { root, pkg },
-);
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+  };
+};
--- example/metro.config.js
+++ example/metro.config.js
@@ -1,37 +1,30 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-const path = require('path');
+const { getDefaultConfig } = require('expo/metro-config');
 
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+const path = require('path');
 const root = path.resolve(__dirname, '..');
 const packagesDir = path.join(root, 'packages');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {
-  watchFolders: [root],
+config.watchFolders = [root];
 
-  resolver: {
-    nodeModulesPaths: [
-      path.join(__dirname, 'node_modules'),
-      path.join(root, 'node_modules'),
-      packagesDir,
-    ],
-    extraNodeModules: {
-      stream: require.resolve('readable-stream'),
-    },
-  },
+config.resolver.nodeModulesPaths = [
+  path.join(__dirname, 'node_modules'),
+  path.join(root, 'node_modules'),
+  packagesDir,
+];
 
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  stream: require.resolve('readable-stream'),
 };
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: false,
+    inlineRequires: true,
+  },
+});
+
+module.exports = config;
