34bb4882d1f0729860c89f4725cf4371ec6658f6
Athex Web3 <athexweb3@gmail.com>
2025-12-19 19:12:53 +0600

    fix(lint): restore standard config and use expo lint

--- examples/native/.eslintrc.js
+++ examples/native/.eslintrc.js
@@ -1,7 +1,14 @@
 module.exports = {
   root: true,
-  extends: 'expo',
+  extends: ['expo', 'prettier'],
+  plugins: ['prettier'],
+  ignorePatterns: ['dist/', 'node_modules/'],
   env: {
     node: true,
+  },
+  rules: {
+    'prettier/prettier': 'warn',
+    'react-native/no-inline-styles': 'off',
+    'react-native/no-color-literals': 'off',
   },
 };
